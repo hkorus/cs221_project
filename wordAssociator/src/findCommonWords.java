2959bdd903a6a4f0ac160cbb0f377883daff23ef
Sam Beder <sambeder@rescomp-12-149442.stanford.edu>
2012-11-25 21:33:30 -0800

    made map to store most common words

--- wordAssociator/src/findCommonWords.java
+++ wordAssociator/src/findCommonWords.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class findCommonWords {
 
@@ -17,13 +20,27 @@ public class findCommonWords {
 	            reader = new BufferedReader(new FileReader(file));
 	            String text = null;
 	 
+                Map<String, Integer> wordCount = new HashMap<String, Integer>();
+
+	            
 	            // repeat until all lines is read
 	            while ((text = reader.readLine()) != null) {
-	            	System.out.println(text);
+	            	//System.out.println(text);
 	                contents.append(text)
 	                        .append(System.getProperty(
 	                                "line.separator"));
+	                
+	                if(wordCount.containsKey(text)) {
+	                	wordCount.put(text, wordCount.get(text)+1);
+	                } else {
+	                	wordCount.put(text, 1);
+	                }
+	                
 	            }
+	            
+	            int test = Collections.max(wordCount.values());
+	            System.out.println(test);
+	            
 	        } catch (FileNotFoundException e) {
 	            e.printStackTrace();
 	        } catch (IOException e) {
@@ -39,7 +56,7 @@ public class findCommonWords {
 	        }
 	 
 	        // show file contents here
-	        System.out.println(contents.toString());
+	        //System.out.println(contents.toString());
 	    }
 	
 	
