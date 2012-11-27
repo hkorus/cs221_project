19714ead86a1badeb59ff17c35a2563744344b6c
Hannah Korus <hkorus@gmail.com>
2012-11-26 22:09:29 -0800

    added some nyt corpora

--- wordAssociator/src/findCommonWords.java
+++ wordAssociator/src/findCommonWords.java
@@ -24,73 +24,80 @@ public class findCommonWords {
 	private static int numNeighbors = 10;
 
 	public static void main(String[] args) {
-		File file = new File("Middlemarch.txt");
-		HashSet<String> stopWords = getStopWords();
-		Scanner scanner = null;
-		Queue<String> neighbors = new ArrayBlockingQueue<String>(numNeighbors);
-		try {
-			scanner = new Scanner(new BufferedReader(new FileReader(file)));
-			scanner.useDelimiter("[^A-Za-z]");
-			String token = null;
-			final Map<String, Integer> wordCount = new HashMap<String, Integer>();
-			final Map<String, Map<String, Integer> > neighborMap = new HashMap<String, Map<String, Integer>>(); 
-			Vector<String> frequent = new Vector<String>();
-			int counter = 0;
+		final Map<String, Integer> wordCount = new HashMap<String, Integer>();
+		final Map<String, Map<String, Integer> > neighborMap = new HashMap<String, Map<String, Integer>>();
+		Vector<String> frequent = new Vector<String>();
+		for (int i = 7; i <= 9 ; i++) {
+			String filename = "nyt_eng_19940" + i + ".txt";
 
-			// repeat until all words are read
-			while (scanner.hasNext()) {
-				token = scanner.next().trim().toLowerCase(); //.replaceAll("[^A-Za-z]", "");
 
-				if(wordCount.containsKey(token)) {
-					wordCount.put(token, wordCount.get(token)+1);
-				} else {
-					wordCount.put(token, 1);
-				}
+			File file = new File(filename);
+			HashSet<String> stopWords = getStopWords();
+			Scanner scanner = null;
+			Queue<String> neighbors = new ArrayBlockingQueue<String>(numNeighbors);
+			try {
+				scanner = new Scanner(new BufferedReader(new FileReader(file)));
+				scanner.useDelimiter("[^A-Za-z]");
+				String token = null;
 
-				if(!frequent.contains(token) && !stopWords.contains(token)){
+				
+				int counter = 0;
 
-					//update neighbors for word that you push (get all the neighbors 10 before the word)
-					//and for the word that you pop (get all the neighbors 10 after the word)
-					updateNeighbors(neighborMap, neighbors, token);
-					if (neighbors.size() < numNeighbors) {
-						neighbors.offer(token);
+				// repeat until all words are read
+				while (scanner.hasNext()) {
+					token = scanner.next().trim().toLowerCase(); //.replaceAll("[^A-Za-z]", "");
+
+					if(wordCount.containsKey(token)) {
+						wordCount.put(token, wordCount.get(token)+1);
 					} else {
-						updateNeighbors(neighborMap, neighbors, neighbors.poll());
-						neighbors.offer(token);
+						wordCount.put(token, 1);
 					}
 
-					if(frequent.size()<40) {
-						frequent.add(token);
-						//printFrequent(frequent, wordCount);
-					} else if(wordCount.get(token)>wordCount.get(frequent.get(frequent.size()-1))) {
+					if(!frequent.contains(token) && !stopWords.contains(token)){
 
-						frequent.remove(frequent.size()-1);
-						frequent.add(token);
-						counter+=1;
-						//System.out.println(counter);
+						//update neighbors for word that you push (get all the neighbors 10 before the word)
+						//and for the word that you pop (get all the neighbors 10 after the word)
+						updateNeighbors(neighborMap, neighbors, token);
+						if (neighbors.size() < numNeighbors) {
+							neighbors.offer(token);
+						} else {
+							updateNeighbors(neighborMap, neighbors, neighbors.poll());
+							neighbors.offer(token);
+						}
 
-						Collections.sort(frequent, new Comparator<String>() {
-							//@Override
-							public int compare(String str1, String str2) {
-								return(wordCount.get(str2) - wordCount.get(str1));
-							}
-						});
+						if(frequent.size()<40) {
+							frequent.add(token);
+							//printFrequent(frequent, wordCount);
+						} else if(wordCount.get(token)>wordCount.get(frequent.get(frequent.size()-1))) {
+
+							frequent.remove(frequent.size()-1);
+							frequent.add(token);
+							counter+=1;
+							System.out.println(token);
+
+							Collections.sort(frequent, new Comparator<String>() {
+								//@Override
+								public int compare(String str1, String str2) {
+									return(wordCount.get(str2) - wordCount.get(str1));
+								}
+							});
+						}
 					}
 				}
-			}
 
-			//printFrequent(frequent, wordCount);
-			printNeighborsSortedByFrequency(neighborMap);
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (scanner != null) {
-				scanner.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (scanner != null) {
+					scanner.close();
+				}
 			}
 		}
+		printFrequent(frequent, wordCount);
+		printNeighborsSortedByFrequency(neighborMap);
+		
 	}
 
 	private static void printNeighborsSortedByFrequency(
@@ -119,16 +126,18 @@ public class findCommonWords {
 	}
 
 	private static void updateNeighbors(Map<String, Map<String, Integer> > neighborMap, Queue<String> neighbors, String token) {
-		if (!neighborMap.containsKey(token)) {
-			neighborMap.put(token, new HashMap<String, Integer>());
-		}
+		if (token.equals("clinton")) {
+			if (!neighborMap.containsKey(token)) {
+				neighborMap.put(token, new HashMap<String, Integer>());
+			}
 
-		for (String neighbor : neighbors) {
-			if (!neighbor.equals(token) && !neighbor.contains(token) && !token.contains(neighbor)) {
-				if (neighborMap.get(token).containsKey(neighbor)) {
-					neighborMap.get(token).put(neighbor, neighborMap.get(token).get(neighbor)+1);
-				} else {
-					neighborMap.get(token).put(neighbor, 1);
+			for (String neighbor : neighbors) {
+				if (!neighbor.equals(token) && !neighbor.contains(token) && !token.contains(neighbor)) {
+					if (neighborMap.get(token).containsKey(neighbor)) {
+						neighborMap.get(token).put(neighbor, neighborMap.get(token).get(neighbor)+1);
+					} else {
+						neighborMap.get(token).put(neighbor, 1);
+					}
 				}
 			}
 		}
@@ -155,8 +164,5 @@ public class findCommonWords {
 		for(int i =0; i<frequent.size(); i++){
 			System.out.println(frequent.get(i) + " count: " + wordCount.get(frequent.get(i)));
 		}
-
 	}
-
-
 }
